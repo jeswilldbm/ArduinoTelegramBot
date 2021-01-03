509f60cbfe676e4a90f64be43bcab31e325f50b2
jeswilldbm <jeswilldbm@gmail.com>
2021-01-03 15:08:24 +0100

    actualizar

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -5,6 +5,13 @@ const token = '1415079723:AAEPAlXFBbzlAQI0qf10Z04ALWpzs4Fl7ys';//Cambiar por el 
 const bot = new TelegramBot(token, {
   polling: true
 });
+var IdMiChat = 616815725;//cambiar por tu ID del chat
+
+var SerialPort = require('serialport');
+var MiPuerto = new SerialPort('COM3', {
+  baudRate: 9600,
+  autoOpen: true
+});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
@@ -13,8 +20,20 @@ bot.on('message', (msg) => {
   if (Mensaje == "Encender") {
     console.log("encendiendo el led");
     bot.sendMessage(chatId, 'Encendere el led');
+    MiPuerto.write("H");
   } else if (Mensaje == "Apagar") {
     console.log("apagar el led");
     bot.sendMessage(chatId, 'Apagare el led');
+    MiPuerto.write("L");
+  }
+});
+
+MiPuerto.setEncoding('utf8');
+
+MiPuerto.on('data', function(data) {
+  console.log("Lo que entro es " + data);
+  if (data[0] == 'H') {
+    console.log("Boton Precionado");
+    bot.sendMessage(IdMiChat, "Se preciono el boton");
   }
 });
