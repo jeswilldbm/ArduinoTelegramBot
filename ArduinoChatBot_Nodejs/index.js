fc88b5b58930ca9534fe0a70737785ef2ddb8fae
jeswilldbm <jeswilldbm@gmail.com>
2021-01-03 16:21:29 +0100

    ACTUALIZAR

--- ArduinoChatBot_Nodejs/index.js
+++ ArduinoChatBot_Nodejs/index.js
@@ -17,33 +17,23 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   console.log("El ID del char" + chatId);
   var Mensaje = msg.text;
-  if (Mensaje == "Encender Rojo") {
-    console.log("encendiendo el led rojo");
-    bot.sendMessage(chatId, 'Encendere el led Rojo');
-    MiPuerto.write("A");
-  } else if (Mensaje == "Apagar Rojo") {
+  if (Mensaje == "Encender") {
+    console.log("encendiendo el led");
+    bot.sendMessage(chatId, 'Encendere el led');
+    MiPuerto.write("H");
+  } else if (Mensaje == "Apagar") {
     console.log("apagar el led");
-    bot.sendMessage(chatId, 'Apagare el led Rojo');
-    MiPuerto.write("B");
+    bot.sendMessage(chatId, 'Apagare el led');
+    MiPuerto.write("L");
   }
-  else if (Mensaje == "Encender amarillo") {
-    console.log("Encendiendo el led amarillo");
-    bot.sendMessage(chatId, 'Encendere el led Amarillo');
-    MiPuerto.write("C");
+});
+
+MiPuerto.setEncoding('utf8');
+
+MiPuerto.on('data', function(data) {
+  console.log("Lo que entro es " + data);
+  if (data[0] == 'H') {
+    console.log("Boton Precionado");
+    bot.sendMessage(IdMiChat, "Se preciono el boton");
   }
-  else if (Mensaje == "Apagar amarillo") {
-    console.log("apagar el led amarillo");
-    bot.sendMessage(chatId, 'Apagare el led Amarillo');
-    MiPuerto.write("D");
-  }
-  else if (Mensaje == "Encender azul") {
-    console.log("encender el led azul");
-    bot.sendMessage(chatId, 'Apagare el led Azul');
-    MiPuerto.write("E");
-  }
-  else if (Mensaje == "Apagar Azul") {
-    console.log("apagar el led azul");
-    bot.sendMessage(chatId, 'Apagare el led azul');
-    MiPuerto.write("F");
-  }
-}
+});
